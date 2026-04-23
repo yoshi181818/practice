f1736b6c3960f400cf4efd1b51b8d66c5ab63b84
田中　慶明 <24110006@oic-ok.ac.jp>
2026-04-23 09:49:41 +0900

    Add files via upload

---
+++ 0420jugyo/src/Boeing787.java
@@ -0,0 +1,10 @@
+class Boeing787 extends Plane{
+	@Override
+	public void run() {
+		System.out.println("ゴゴゴと走る");
+	}
+	@Override
+	public void fly() {
+		System.out.println("ビューンと飛ぶ");
+	}
+}
---
+++ 0420jugyo/src/WrightFlyer.java
@@ -0,0 +1,11 @@
+
+public class WrightFlyer extends Plane{
+	@Override
+	public void run() {
+		System.out.println("バタバタ走る");
+	}
+	@Override
+	public void fly() {
+		System.out.println("ブーンと飛ぶ");
+	}
+}
